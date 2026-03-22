049de78e2f0647c234752f9cd469e3d5f103f616
Abhisheik Motee <abhisheik.motee@redboxdigital.com>
2026-03-22 12:27:12 +0400

    Revert "Move AI categorization into browser"
    This reverts commit b05999c524aad5bcc0d6c0152449a7cc975fa807.

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
     trace: "on-first-retry",
   },
   webServer: {
-    command: "python3 -m http.server 4173",
+    command: "node server.js",
     url: "http://127.0.0.1:4173",
     reuseExistingServer: true,
     timeout: 30_000,
